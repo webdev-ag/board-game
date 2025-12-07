f9814efd8124b260ccc5d9d50feda86cf5be6bd2
dominikschopper <github@dschopper.de>
2025-12-07 19:20:32 +0100

    make movement wordk

--- src/scripts/main.mjs
+++ src/scripts/main.mjs
@@ -1,10 +1,10 @@
 import { boardMaps, mapOrder } from './maps/index.mjs';
 
 /**
- *
+ * @param {Array<Array<string>>|undefined} newMap
  * @returns {map: Array<string[]>, heroPos: { heroRow: number, heroCol: number }}
  */
-function drawBoard(newMap = undefined) {
+function drawGame(newMap = undefined) {
   const game = document.querySelector('#game');
 
   const rowsVariableName = '--board-rows';
@@ -27,6 +27,7 @@ function drawBoard(newMap = undefined) {
  * @returns {{ heroRow: number, heroCol: number }}
  */
 function renderMap(insertHere, map) {
+  insertHere.innerHTML = '';
   const heroPos = { heroRow: -1, heroCol: -1 };
   for (const r in map) {
     const row = map[r];
@@ -34,7 +35,7 @@ function renderMap(insertHere, map) {
       const cell = row[c];
       const cellElement = document.createElement('div');
       insertHere.appendChild(cellElement);
-      cellElement.classList.add('cell');
+      cellElement.classList.add('cell', `col-${c}`, `row-${r}`);
       if (cell === '#') {
         cellElement.classList.add('wall');
       } else if (cell === 'X') {
@@ -49,36 +50,87 @@ function renderMap(insertHere, map) {
   return heroPos;
 }
 
+/**
+ *
+ * @param {{heroRow:number, heroCol:number}} heroPos
+ * @param {Array<Arra<string>>} map
+ * @returns
+ */
 function moveHeroUp(heroPos, map) {
   const { heroCol, heroRow } = heroPos;
   const moveToCol = heroCol;
   const moveToRow = heroRow - 1;
-  if (moveToRow - 1 < 0) {
-    return;
+    if (moveToRow - 1 < 0) {
+    return {heroPos, map};
   }
   const goToCell = map[moveToRow][moveToCol];
   if (goToCell === ' ') {
     map[moveToRow][moveToCol] = 'X';
-    map[heroRow][heroCol] = 'E';
+    map[heroRow][heroCol] = ' ';
+    drawGame(map);
+    return { map, heroPos: { heroRow: moveToRow, heroCol: moveToCol } };
   }
-  drawBoard(map);
-  return { map, heroPos: { heroRow: moveToRow, heroCol: moveToCol } };
+  return {heroPos, map};
 }
 
+/**
+ *
+ * @param {{heroRow:number, heroCol:number}} heroPos
+ * @param {Array<Arra<string>>} map
+ * @returns
+ */
 function moveHeroDown(heroPos, map) {
   const { heroCol, heroRow } = heroPos;
-  const moveToCol = heroCol;
-  const moveToRow = heroRow + 1;
-  if (moveToRow + 1 >= map.length) {
-    return;
+  const moveToCol = Number(heroCol);
+  const moveToRow = Number(heroRow) + 1;
+    if (moveToRow + 1 > map.length) {
+    return {heroPos, map};
   }
+
   const goToCell = map[moveToRow][moveToCol];
   if (goToCell === ' ') {
     map[moveToRow][moveToCol] = 'X';
-    map[heroRow][heroCol] = 'E';
+    map[heroRow][heroCol] = ' ';
+    drawGame(map);
+    return { map, heroPos: { heroRow: moveToRow, heroCol: moveToCol } };
   }
-  drawBoard(map);
-  return { map, heroPos: { heroRow: moveToRow, heroCol: moveToCol } };
+  return {heroPos, map};
+}
+
+function moveHeroLeft(heroPos, map) {
+  const { heroCol, heroRow } = heroPos;
+  const moveToCol = Number(heroCol) - 1;
+  const moveToRow = Number(heroRow);
+    if (moveToCol < 0) {
+    return {heroPos, map};
+  }
+
+  const goToCell = map[moveToRow][moveToCol];
+  if (goToCell === ' ') {
+    map[moveToRow][moveToCol] = 'X';
+    map[heroRow][heroCol] = ' ';
+    drawGame(map);
+    return { map, heroPos: { heroRow: moveToRow, heroCol: moveToCol } };
+  }
+  return {heroPos, map};
+}
+
+function moveHeroRight(heroPos, map) {
+  const { heroCol, heroRow } = heroPos;
+  const moveToCol = Number(heroCol) + 1;
+  const moveToRow = Number(heroRow);
+    if (moveToCol + 1 > map[moveToRow].length) {
+    return {heroPos, map};
+  }
+
+  const goToCell = map[moveToRow][moveToCol];
+  if (goToCell === ' ') {
+    map[moveToRow][moveToCol] = 'X';
+    map[heroRow][heroCol] = ' ';
+    drawGame(map);
+    return { map, heroPos: { heroRow: moveToRow, heroCol: moveToCol } };
+  }
+  return {heroPos, map};
 }
 
 /**
@@ -96,41 +148,48 @@ function setHeroPosition(col, row) {
   cell.classList.add('hero');
 }
 
-function moveHeroLeft() {
-  const { col, row, element } = findHeroPosition();
-  if (col - 1 < 0) {
-    return;
-  }
-  element.classList.remove('hero');
-  setHeroPosition(col - 1, row);
-}
+let { map, heroPos } = drawGame();
 
-function moveHeroRight() {
-  const { col, row, element } = findHeroPosition();
-  if (col + 1 > cols) {
-    return;
-  }
-  element.classList.remove('hero');
-  setHeroPosition(col + 1, row);
-}
+document.querySelector('#move-up').addEventListener('click', () => {
+  const newData = moveHeroUp(heroPos,map)
+  map = newData.map;
+  heroPos = newData.heroPos;
+});
+document.querySelector('#move-down').addEventListener('click', () => {
+  const newData = moveHeroDown(heroPos,map);
+  map = newData.map;
+  heroPos = newData.heroPos;
+});
 
-const { map, heroPos } = drawBoard();
+document.querySelector('#move-left').addEventListener('click', () => {
+  const newData = moveHeroLeft(heroPos,map);
+  map = newData.map;
+  heroPos = newData.heroPos;
+});
 
-document.querySelector('#move-up').addEventListener('click', () => moveHeroUp(map, heroPos));
-document.querySelector('#move-down').addEventListener('click', () => moveHeroDown(map, heroPos));
-
-document.querySelector('#move-left').addEventListener('click', () => moveHeroLeft());
-document.querySelector('#move-right').addEventListener('click', () => moveHeroRight());
+document.querySelector('#move-right').addEventListener('click', () => {
+  const newData = moveHeroRight(heroPos,map)
+  map = newData.map;
+  heroPos = newData.heroPos;
+});
 
 document.addEventListener('keyup', (event) => {
   event.preventDefault();
   if (event.key === 'ArrowDown') {
-    moveHeroDown();
+    const newData = moveHeroDown(heroPos,map);
+    map = newData.map;
+    heroPos = newData.heroPos;
   } else if (event.key === 'ArrowUp') {
-    moveHeroUp();
+    const newData = moveHeroUp(heroPos,map)
+    map = newData.map;
+    heroPos = newData.heroPos;
   } else if (event.key === 'ArrowRight') {
-    moveHeroRight();
+    const newData = moveHeroRight(heroPos,map)
+    map = newData.map;
+    heroPos = newData.heroPos;
   } else if (event.key === 'ArrowLeft') {
-    moveHeroLeft();
+      const newData = moveHeroLeft(heroPos,map);
+      map = newData.map;
+      heroPos = newData.heroPos;
   }
 });
